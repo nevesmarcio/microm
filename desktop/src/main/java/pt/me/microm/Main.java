df43e9a9b22db84d1a9e154e58c2fc458531f732
nevesmarcio <nevesmarcio@gmail.com>
2016-08-28 01:31:32 +0200

    Refactored Android module structure All the libs except libgdx are now managed via  repo as opposed to be locally resident in the libs folder

--- desktop/src/main/java/pt/me/microm/Main.java
+++ desktop/src/main/java/pt/me/microm/Main.java
@@ -16,22 +16,14 @@ public class Main {
 		LwjglApplicationConfiguration cfg = new LwjglApplicationConfiguration();
 		cfg.title = "seeds-game";
 		cfg.useGL20 = false;
-		cfg.width = 1280;//1024;//1280;//1600;//800;// 240 480 480 800
-		cfg.height = 800;//768;//800;//1200;//480;// 320 800 854 1280
+		cfg.width = 800;//1024;//1280;//1600;//800;// 240 480 480 800
+		cfg.height = 480;//768;//800;//1200;//480;// 320 800 854 1280
 		cfg.vSyncEnabled = true;
 		cfg.fullscreen = false;
 		cfg.samples = 1;
 
 		Logger logger = LoggerFactory.getLogger(Main.class.getSimpleName());
 		logger.debug("Hello world.");
-
-        ClassLoader cl = ClassLoader.getSystemClassLoader();
-
-        URL[] urls = ((URLClassLoader)cl).getURLs();
-
-        for(URL url: urls){
-            System.out.println(url.getFile());
-        }
 
 		new LwjglApplication(new GameMicroM(), cfg);
 	}
