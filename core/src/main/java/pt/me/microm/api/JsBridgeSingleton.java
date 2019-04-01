b3222a9b57af61c6e29a7e03257e336c7b2479b5
nevesmarcio <nevesmarcio@gmail.com>
2019-04-01 08:58:06 +0200

    gson and rhino version bump forced jvm args to desktop module

--- core/src/main/java/pt/me/microm/api/JsBridgeSingleton.java
+++ core/src/main/java/pt/me/microm/api/JsBridgeSingleton.java
@@ -25,226 +25,218 @@ import java.util.Iterator;
 
 /**
  * Da forma que tenho esta classe implementada estou a contar com o thread do UI para pendurar o contexto do javascript, o que não me parece fixe!
- * 
- * @author mneves
  *
+ * @author mneves
  */
 public class JsBridgeSingleton implements IEventListener, Disposable {
-	
-	private static final String TAG = JsBridgeSingleton.class.getSimpleName();
-	private static final Logger logger = LoggerFactory.getLogger(TAG);
 
-	private static JsBridgeSingleton instance = null;
+    private static final String TAG = JsBridgeSingleton.class.getSimpleName();
+    private static final Logger logger = LoggerFactory.getLogger(TAG);
 
-	private Context cx;
-	private Scriptable scope;
-	private Object result;
+    private static JsBridgeSingleton instance = null;
 
-	private Thread t;
-	
-	private WorldModel wm;
-	
-	protected JsBridgeSingleton(WorldModel wm) {
-		// Exists only to defeat instantiation.
+    private Context cx;
+    private Scriptable scope;
+    private Object result;
 
-		this.wm = wm;
-		
-		wm.getWorldPhysicsManager().myContactListener.addListener(MyContactListener.EventType.ON_COLLISION_BEGIN, JsBridgeSingleton.this);
-		wm.getWorldPhysicsManager().myContactListener.addListener(MyContactListener.EventType.ON_COLLISION_END, JsBridgeSingleton.this);		
-		
-		m = wm.getPlayer();
-		
-		/*Example invocation of javascript engine!*/
-		GameTickGenerator.PostRunnable(new Runnable() {
+    private Thread t;
 
-			@Override
-			public void run() {
-				// javascript engine
-				cx = Context.enter();
-				cx.setOptimizationLevel(-1); // do not compile - or else it won't run on dalvik vm
-				scope = cx.initStandardObjects();
+    private WorldModel wm;
 
-				Object wrappedOut = Context.javaToJS(/*
-													 * ClassicSingleton.getInstance
-													 * ()
-													 */JsBridgeSingleton.this, scope);
-				ScriptableObject.putProperty(scope, "cs", wrappedOut);
+    protected JsBridgeSingleton(WorldModel wm) {
+        // Exists only to defeat instantiation.
 
-				result = cx.evaluateString(scope,
-						"function f(x){return x+1}; f(7);", "somescript.js", 1,
-						null); // 1 is the line number!
+        this.wm = wm;
 
-				logger.info(">>>>>>>" + Context.toString(result));
+        wm.getWorldPhysicsManager().myContactListener.addListener(MyContactListener.EventType.ON_COLLISION_BEGIN, JsBridgeSingleton.this);
+        wm.getWorldPhysicsManager().myContactListener.addListener(MyContactListener.EventType.ON_COLLISION_END, JsBridgeSingleton.this);
 
-			}
-		});
-		
-		/*Raises a thread that listens to console input and evaluates javascript with rhino engine*/
-		if (GameMicroM.FLAG_DEV_ELEMENTS_A) {
-			t = new Thread(new Runnable() {
-				
-				@Override
-				public void run() {
-					InputStreamReader inputStreamReader = new InputStreamReader(System.in);
-					BufferedReader stdin = new BufferedReader(inputStreamReader);
-					int i = 0;
-					try {
-						while (true) {
-							i += 1;
-							final String s;
-							if (stdin.ready()) {
-								s = stdin.readLine();
-								if (s.equals("exit"))
-									break;
-								final Integer in = new Integer(i);
-								GameTickGenerator.PostRunnable(new Runnable() {
-									@Override
-									public void run() {
-										result = cx.evaluateString(scope, s, "<<from console>>", in, null); // 1 is the line number!
-										logger.info(">>>>>>>" + Context.toString(result));
-									}
-								});
-							} else {
-								Thread.sleep(500); // This is not optimized, but this way, the thread is interruptible!
-							}
-						}
-					} catch (InterruptedException ie) { 
-						if (logger.isWarnEnabled()) logger.warn("Its cool! Thread was interrupted by developer: ", ie);
-					} catch (Exception e) {
-						if (logger.isErrorEnabled()) logger.error("Fuck!! What did just happen? ", e);
-						throw new RuntimeException("Exception " + e.getMessage() + " was thrown");
-					} 
-					
-					
-					//FIXME: deverá ser feito um exit() ao context, mas o problema do cleanup n facilita
+        m = wm.getPlayer();
+
+        /*Example invocation of javascript engine!*/
+        GameTickGenerator.PostRunnable(new Runnable() {
+
+            @Override
+            public void run() {
+                // javascript engine
+                cx = Context.enter();
+                cx.setOptimizationLevel(-1); // do not compile - or else it won't run on dalvik vm
+                scope = cx.initStandardObjects();
+
+                Object wrappedOut = Context.javaToJS(/*
+                 * ClassicSingleton.getInstance
+                 * ()
+                 */JsBridgeSingleton.this, scope);
+                ScriptableObject.putProperty(scope, "cs", wrappedOut);
+
+                result = cx.evaluateString(scope,
+                        "function f(x){return x+1}; f(7);", "somescript.js", 1,
+                        null); // 1 is the line number!
+
+                logger.info(">>>>>>>" + Context.toString(result));
+
+            }
+        });
+
+        /*Raises a thread that listens to console input and evaluates javascript with rhino engine*/
+        if (GameMicroM.FLAG_DEV_ELEMENTS_A) {
+            t = new Thread(new Runnable() {
+
+                @Override
+                public void run() {
+                    InputStreamReader inputStreamReader = new InputStreamReader(System.in);
+                    BufferedReader stdin = new BufferedReader(inputStreamReader);
+                    int i = 0;
+                    try {
+                        while (true) {
+                            i += 1;
+                            final String s;
+                            if (stdin.ready()) {
+                                s = stdin.readLine();
+                                if (s.equals("exit"))
+                                    break;
+                                final Integer in = new Integer(i);
+                                GameTickGenerator.PostRunnable(new Runnable() {
+                                    @Override
+                                    public void run() {
+                                        result = cx.evaluateString(scope, s, "<<from console>>", in, null); // 1 is the line number!
+                                        logger.info(">>>>>>>" + Context.toString(result));
+                                    }
+                                });
+                            } else {
+                                Thread.sleep(500); // This is not optimized, but this way, the thread is interruptible!
+                            }
+                        }
+                    } catch (InterruptedException ie) {
+                        if (logger.isWarnEnabled()) logger.warn("Its cool! Thread was interrupted by developer: ", ie);
+                    } catch (Exception e) {
+                        if (logger.isErrorEnabled()) logger.error("Fuck!! What did just happen? ", e);
+                        throw new RuntimeException("Exception " + e.getMessage() + " was thrown");
+                    }
+
+
+                    //FIXME: deverá ser feito um exit() ao context, mas o problema do cleanup n facilita
 //					GameTickGenerator.PostRunnable(new Runnable() {
 //						@Override
 //						public void run() {
 //							Context.exit();
 //						}
 //					});
-										
-	
-				}
-			});
-			
-			t.setName("_javascript_read_from_console");
-			t.start();
-		}
-	}
-
-	public static JsBridgeSingleton getInstance(WorldModel wm) {
-		if (instance == null) {
-			instance = new JsBridgeSingleton(wm);
-			
-		}
-		return instance;
-	}
-	
 
 
-	/**
-	 * Method that should be called when the collisions handler dispatches events!
-	 * This is the method that should depend on the models and not the other way around!!
-	 */	
-	@Override
-	public void onEvent(final IEvent event) {
+                }
+            });
 
-		logger.debug("event:: _" + event.getType() + "_ " + ((CollisionEvent)event).getA() + "<-->" + ((CollisionEvent)event).getB());
-		
-		GameTickGenerator.PostRunnable(new Runnable() {
-			
-			@Override
-			public void run() {
+            t.setName("_javascript_read_from_console");
+            t.start();
+        }
+    }
 
-				ScriptableObject.putProperty(scope, "__EVENT_TYPE", event.getType().toString());
-				ScriptableObject.putProperty(scope, "__ACTOR_A", ((CollisionEvent)event).getA());
-				ScriptableObject.putProperty(scope, "__ACTOR_B", ((CollisionEvent)event).getB());
-				
-				String script = ((CollisionEvent)event).getScript();
+    public static JsBridgeSingleton getInstance(WorldModel wm) {
+        if (instance == null) {
+            instance = new JsBridgeSingleton(wm);
+
+        }
+        return instance;
+    }
+
+
+    /**
+     * Method that should be called when the collisions handler dispatches events!
+     * This is the method that should depend on the models and not the other way around!!
+     */
+    @Override
+    public void onEvent(final IEvent event) {
+
+        logger.debug("event:: _" + event.getType() + "_ " + ((CollisionEvent) event).getA() + "<-->" + ((CollisionEvent) event).getB());
+
+        GameTickGenerator.PostRunnable(new Runnable() {
+
+            @Override
+            public void run() {
+
+                ScriptableObject.putProperty(scope, "__EVENT_TYPE", event.getType().toString());
+                ScriptableObject.putProperty(scope, "__ACTOR_A", ((CollisionEvent) event).getA());
+                ScriptableObject.putProperty(scope, "__ACTOR_B", ((CollisionEvent) event).getB());
+
+                String script = ((CollisionEvent) event).getScript();
 //				script = "function add(a,b){return a + '<--->' + b;}; " +
 //						"cs.out(\"puff>>> \" + add(__ACTOR_A, __ACTOR_B));" +
 //						"if (__EVENT_TYPE.indexOf(\"ON_COLLISION_BEGIN\") !== -1)" +
 //						"if ((__ACTOR_A.indexOf(\"trigger\") !== -1) || (__ACTOR_B.indexOf(\"trigger\") !== -1)) cs.m.jump();";
-				
-				if (script != null)
-					JsBridgeSingleton.this.cx.evaluateString(scope, script, "external", 1, null);
-			}
-		});
-		
-	}
-	   
-	@Override
-	public void dispose() {
-		if (GameMicroM.FLAG_DEV_ELEMENTS_A)
-			t.interrupt();
-		
-		m = null;
-		instance = null;
-	}	   
 
-	   
+                if (script != null)
+                    JsBridgeSingleton.this.cx.evaluateString(scope, script, "external", 1, null);
+            }
+        });
 
-	/************ EXPOSED API ************/
-	public DaBoxModel m;
+    }
 
-	public void out(String s) {
-		logger.info(s);
-	}
+    @Override
+    public void dispose() {
+        if (GameMicroM.FLAG_DEV_ELEMENTS_A)
+            t.interrupt();
 
-	
-	/**
-	 * method to list objects by name
-	 */
-	public void listBodies() {
-	    Array<Body> bodies = new Array<Body>();
-        wm.getWorldPhysicsManager().getPhysicsWorld().getBodies(bodies);
+        m = null;
+        instance = null;
+    }
 
-        Iterator<Body> it = bodies.iterator();
-		out("start listing...");
-		while (it.hasNext()) {
-			Body b = it.next();
-			String name = ((AbstractModel) b.getUserData()).getName();
-			try {
-				out("\t.." + name);
-			} catch (Exception e) {
-				e.printStackTrace();
-			}
-		}			
-		out("end listing...");
-	}
-	
-	/**
-	 * method to lookup for an object given its name
-	 */
-	public Body findBodyByName(String searchName) {
+
+    /************ EXPOSED API ************/
+    public DaBoxModel m;
+
+    public void out(String s) {
+        logger.info(s);
+    }
+
+
+    /**
+     * method to list objects by name
+     */
+    public void listBodies() {
         Array<Body> bodies = new Array<Body>();
         wm.getWorldPhysicsManager().getPhysicsWorld().getBodies(bodies);
 
-	    Iterator<Body> it = bodies.iterator();
+        Iterator<Body> it = bodies.iterator();
+        out("start listing...");
+        while (it.hasNext()) {
+            Body b = it.next();
+            String name = ((AbstractModel) b.getUserData()).getName();
+            try {
+                out("\t.." + name);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+        out("end listing...");
+    }
 
-		while (it.hasNext()) {
-			Body b = it.next();
-			String name = ((AbstractModel) b.getUserData()).getName();
-			try {
-				logger.debug("searching...  [" + name + "]");
-				if (name.equals(searchName)) 
-					return b;
-			} catch (Exception e) {
-				e.printStackTrace();
-			}
-		}			
+    /**
+     * method to lookup for an object given its name
+     */
+    public Body findBodyByName(String searchName) {
+        Array<Body> bodies = new Array<Body>();
+        wm.getWorldPhysicsManager().getPhysicsWorld().getBodies(bodies);
 
-		return null;
-	}
+        Iterator<Body> it = bodies.iterator();
 
-	
-	
-	/******** END OF EXPOSED API *********/
-	
-	
-	
-	   
+        while (it.hasNext()) {
+            Body b = it.next();
+            String name = ((AbstractModel) b.getUserData()).getName();
+            try {
+                logger.debug("searching...  [" + name + "]");
+                if (name.equals(searchName))
+                    return b;
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
 
-	   
-	}
+        return null;
+    }
+
+
+    /******** END OF EXPOSED API *********/
+
+
+}
--- desktop/src/main/java/pt/me/microm/Main.java
+++ desktop/src/main/java/pt/me/microm/Main.java
@@ -6,30 +6,29 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 
-
 public class Main {
-	public static double JAVA_VERSION = getVersion ();
+    static double getVersion() {
+        String version = System.getProperty("java.version");
+        int pos = version.indexOf('.');
+        pos = version.indexOf('.', pos + 1);
+        return Double.parseDouble(version.substring(0, pos));
+    }
 
-	static double getVersion () {
-		String version = System.getProperty("java.version");
-		int pos = version.indexOf('.');
-		pos = version.indexOf('.', pos+1);
-		return Double.parseDouble (version.substring (0, pos));
-	}
+    public static double JAVA_VERSION = getVersion();
 
-	public static void main(String[] args) {
-		LwjglApplicationConfiguration cfg = new LwjglApplicationConfiguration();
-		cfg.title = "seeds-game";
-		cfg.width = 800;//320;//1024;//1280;//1600;//800;// 240 480 480 800
-		cfg.height = 480;//240;//768;//800;//1200;//480;// 320 800 854 1280
-		cfg.vSyncEnabled = true;
-		cfg.fullscreen = false;
-		cfg.samples = 1;
+    public static void main(String[] args) {
+        LwjglApplicationConfiguration cfg = new LwjglApplicationConfiguration();
+        cfg.title = "seeds-game";
+        cfg.width = 800;//320;//1024;//1280;//1600;//800;// 240 480 480 800
+        cfg.height = 480;//240;//768;//800;//1200;//480;// 320 800 854 1280
+        cfg.vSyncEnabled = true;
+        cfg.fullscreen = false;
+        cfg.samples = 1;
 
-		Logger logger = LoggerFactory.getLogger(Main.class.getSimpleName());
-		logger.debug("Hello world.");
-		logger.info("JAVA_VERSION: " + JAVA_VERSION);
+        Logger logger = LoggerFactory.getLogger(Main.class.getSimpleName());
+        logger.debug("Hello world.");
+        logger.info("JAVA_VERSION: " + JAVA_VERSION);
 
-		new LwjglApplication(new GameMicroM(), cfg);
-	}
+        new LwjglApplication(new GameMicroM(), cfg);
+    }
 }
